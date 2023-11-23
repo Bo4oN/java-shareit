e2d63a3061341796337792273a3a6ebdef69c6d8
Bo4oN <obliviom99@yandex.ru>
2023-11-23 09:02:21 +0300

    Стилистические изменения

--- src/main/java/ru/practicum/shareit/item/ItemRepository.java
+++ src/main/java/ru/practicum/shareit/item/ItemRepository.java
@@ -7,7 +7,7 @@ import ru.practicum.shareit.item.model.Item;
 import java.util.List;
 
 public interface ItemRepository extends JpaRepository<Item, Integer> {
-    List<Item> findByOwnerId(int owner_id);
+    List<Item> findByOwnerId(int ownerId);
 
     @Query("select i from Item as i " +
             "where (upper(i.name) like upper(concat('%', ?1, '%')) " +
